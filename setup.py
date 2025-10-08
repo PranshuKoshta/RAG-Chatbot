9abf42599e6ec355a18b13b3c2a3b8bd2dca8ec3
Pranshu Koshta <pranshukoshta7@gmail.com>
2025-10-08 17:12:22 +0530

    requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="RAG_chatbot",
+    version="0.1.0",
+    author="Pranshu Koshta",
+    author_email="pranshukoshta7@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
